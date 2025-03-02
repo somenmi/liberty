35407f4c83f23f12be7be6d48b0744231819a605
COMXAJLTA <119046696+somenmi@users.noreply.github.com>
2025-03-02 10:06:06 +0300

    Убрал лишнее script.js

--- script.js
+++ script.js
@@ -15,31 +15,6 @@ const infoData = {
 // Обновляем текст на странице
 document.getElementById('info-text').textContent = infoData.description;
 
-// Функция для проверки видимости элемента, то есть про исчезновение страницы
-function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-    );
-}
-
-// Функция для обработки скролла, для исчезновение страницы
-function handleScroll() {
-    const items = document.querySelectorAll('.release-item');
-    items.forEach(item => {
-        if (isElementInViewport(item)) {
-            item.classList.add('visible');
-        }
-    });
-}
-
-// Добавляем обработчик события скролла, для исчезновение страницы
-window.addEventListener('scroll', handleScroll);
-
-// Вызываем handleScroll при загрузке страницы, чтобы проверить видимость элементов, для исчезновение страницы
-window.addEventListener('load', handleScroll);
-
 // ТОЛЬКО ПО ГРАДИЕНТУ, чтобы появлялапсь только при скроле
 window.addEventListener('scroll', () => {
     const fadeTop = document.querySelector('.fade-top');
